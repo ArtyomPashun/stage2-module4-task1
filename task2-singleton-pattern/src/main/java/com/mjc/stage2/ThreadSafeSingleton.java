2b1a8d240b4f859f3ddeb541e0042843095685d9
Artsiom Pashun <artyom.pashun@gmail.com>
2023-07-20 16:24:13 +0200

    Thread safe singleton was created

--- task2-singleton-pattern/src/main/java/com/mjc/stage2/ThreadSafeSingleton.java
+++ task2-singleton-pattern/src/main/java/com/mjc/stage2/ThreadSafeSingleton.java
@@ -1,5 +1,13 @@
 package com.mjc.stage2;
 
 public class ThreadSafeSingleton {
-    // Write your code here!
+
+    public static final ThreadSafeSingleton INSTANCE = new ThreadSafeSingleton();
+
+    public static ThreadSafeSingleton getInstance() {
+        return INSTANCE;
+    }
+
+    private ThreadSafeSingleton() {
+    }
 }
